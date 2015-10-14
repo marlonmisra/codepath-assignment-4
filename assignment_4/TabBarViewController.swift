4c8443dea52d421c4062003e1f4c3985f3225605
marlonmisra <marlonmisra@gmail.com>
2015-10-14 15:31:54 -0700

    initia commit

---
+++ assignment_4/TabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarViewController.swift
+//  assignment_4
+//
+//  Created by Marlon Misra on 10/14/15.
+//  Copyright © 2015 Marlon Misra. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ assignment_4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment_4
+//
+//  Created by Marlon Misra on 10/14/15.
+//  Copyright © 2015 Marlon Misra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
